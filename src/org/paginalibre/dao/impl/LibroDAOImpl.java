cfeae78b164ff2047a0c3f6447c75804ca338c96
Herberth Cuyuch <hcuyuch-2026046@kinal.edu.gt>
2026-08-27 11:52:34 -0600

    Progreso para el CRUD, DAO y DAOImpl de Libro

---
+++ src/org/paginalibre/dao/impl/LibroDAOImpl.java
@@ -0,0 +1,95 @@
+package org.paginalibre.dao.impl;
+import org.paginalibre.util.Conexion;
+import org.paginalibre.model.Libro;
+import org.paginalibre.dao.LibroDAO;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+        
+public class LibroDAOImpl implements LibroDAO{
+    @Override
+    public boolean insertar(Libro libros) {
+     String consulta = "{call sp_insertarlibro(?, ?, ?, ?)}";
+        try (Connection conexion = Conexion.getInstancia().conectar();
+             CallableStatement consultaCall = conexion.prepareCall(consulta)) {          
+            consultaCall.setString(1, libros.getIsbn());
+            consultaCall.setString(2, libros.getTitulo());
+            consultaCall.setDate(3, java.sql.Date.valueOf(libros.getFechaPublicacion()));
+            consultaCall.setDouble(4, libros.getPrecio());
+            consultaCall.setInt(5, libros.getIdCategoria());
+            consultaCall.setString(6, libros.getNitEditorial());
+            consultaCall.setInt(7, libros.getStockActual());
+            consultaCall.setInt(8, libros.getStockMinimo());
+            consultaCall.setBoolean(9, libros.isActivo());
+            consultaCall.setTimestamp(10, libros.getFechaActualizacion());
+            
+            return consultaCall.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.err.print("Error al crear Libro: " + e.getMessage());
+            return false;
+        }
+    }
+    @Override
+    public List<Libro> listar() {      
+    List<Libro> lista = new ArrayList<>();
+    String consulta = "{call sp_listarlibro()}";              
+    
+    try (Connection conexion = Conexion.getInstancia().conectar(); 
+         CallableStatement consultaCall = conexion.prepareCall(consulta); 
+         ResultSet tablaResultado = consultaCall.executeQuery();) {
+       
+          while(tablaResultado.next()){
+             lista.add(new Libro(
+
+        tablaResultado.getString("isbn"),
+        tablaResultado.getString("titulo"),sqlDate != null ? sqlDate.toLocalDate() : null, // Conversión a LocalDate
+        tablaResultado.getDouble("precio"),
+        tablaResultado.getInt("id_categoria"),
+        tablaResultado.getString("nit_editorial")
+    ));
+          }
+    }catch (SQLException e){
+        System.err.println("ERROR al listar Autores:" + e.getMessage());
+        e.printStackTrace();
+    }        
+    return Libro;
+}
+
+@Override
+public Autores buscarPorId(int id_autor) {
+    Autores autor = new Autores();
+    String consultaSQL = "{call sp_buscarautor(?)}";
+    try (Connection conexion = Conexion.getInstancia().conectar();
+         CallableStatement consultaCall = conexion.prepareCall(consultaSQL);) {
+        consultaCall.setInt(1, id_autor);
+        try (ResultSet tablaResultado = consultaCall.executeQuery()) {
+            if (tablaResultado.next()) {
+                autor.setIdAutor(tablaResultado.getInt("id_autor"));
+                autor.setNombreAutor(tablaResultado.getString("nombre_autor"));
+                autor.setApellidoAutor(tablaResultado.getString("apellido_autor"));
+                autor.setNacionalidad(tablaResultado.getString("nacionalidad"));
+                autor.setBiografia(tablaResultado.getString("biografia"));
+            } else {
+                return null;
+            }
+        }
+    } catch (SQLException e) {
+        System.err.print("Error al buscar Autor: " + e.getMessage());
+        e.printStackTrace();
+    }
+    return autor;
+}
+        
+    @Override
+    public boolean actualizar(Autores autores) {
+        return false;
+    }
+    @Override
+    public boolean eliminar(int idAutores) {
+        return false;
+    }
+}
