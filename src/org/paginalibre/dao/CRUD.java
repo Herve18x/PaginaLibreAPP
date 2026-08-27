3672dc51a7bbef054db3ef716ae83a622698ad7d
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-08-27 09:20:31 -0600

    Implementacion del CRUD #1

---
+++ src/org/paginalibre/dao/CRUD.java
@@ -0,0 +1,13 @@
+
+package org.paginalibre.dao;
+
+
+import java.util.List;
+
+public interface CRUD<T, ID> {
+    boolean insertar(T objeto);
+    List<T> listar();
+    T buscar(ID id);
+    boolean actualizar(T objeto);
+    boolean eliminar(ID id);
+}
