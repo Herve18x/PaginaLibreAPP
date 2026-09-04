1f2c3491d02eb508e630a7b771d2eb1c5a4b0e93
AndreeZabala <azabala-2023347@kinal.edu.gt>
2026-09-04 09:08:16 -0600

    Remove comment on buscarPorNombre method
    Removed commented line about custom query for searching authors by name.

--- src/org/paginalibre/dao/AutorDAO.java
+++ src/org/paginalibre/dao/AutorDAO.java
@@ -4,6 +4,5 @@ import java.util.List;
 import org.paginalibre.model.Autor;
 
 public interface AutorDAO extends CRUD<Autor, Integer> {
-    // Consulta personalizada para buscar autores por coincidencia en el nombre
     List<Autor> buscarPorNombre(String nombre);
 }
